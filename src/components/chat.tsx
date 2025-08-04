2a3c6d907324e1ae872e499bd6bc2ae0ab388d2c
oleyva93 <oleyva930424@gmail.com>
2025-08-03 20:13:47 -0600

    refactor: update chat component structure and enhance functionality with GroupChannelProvider

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,5 +1,9 @@
 import "@sendbird/uikit-react/dist/index.css";
 import GroupChannel from "@sendbird/uikit-react/GroupChannel";
+import {
+  GroupChannelProvider,
+  useGroupChannelContext,
+} from "@sendbird/uikit-react/GroupChannel/context";
 import React, { memo, useState } from "react";
 
 import type { ChatWindowProps } from "../types";
@@ -7,29 +11,39 @@ import { ChatSettingsSection } from "./chat-settings-section";
 import { DragResize } from "./ui/drag-resize";
 
 export const Chat: React.FC<ChatWindowProps> = memo(({ channelUrl }) => {
-  const [showSettings, setShowSettings] = useState(false);
   if (!channelUrl) {
     return null;
   }
 
   return (
     <DragResize>
-      <div className="relative w-full h-full rounded-xl bg-white p-1 border border-gray-200">
-        <GroupChannel
-          channelUrl={channelUrl}
-          key={channelUrl}
-          onChatHeaderActionClick={() => setShowSettings(true)}
-        />
-
-        {showSettings && (
-          <div className="absolute top-0 right-0 w-full h-full bg-black/50 flex justify-end rounded-xl">
-            <ChatSettingsSection
-              channelUrl={channelUrl}
-              onClose={() => setShowSettings(false)}
-            />
-          </div>
-        )}
-      </div>
+      <GroupChannelProvider channelUrl={channelUrl}>
+        <ChanelSection channelUrl={channelUrl} />
+      </GroupChannelProvider>
     </DragResize>
   );
 });
+
+function ChanelSection({ channelUrl }: { channelUrl: string }) {
+  const [showSettings, setShowSettings] = useState(false);
+  const { currentChannel } = useGroupChannelContext();
+
+  return (
+    <div className="relative w-full h-full rounded-xl bg-white p-1 border border-gray-200">
+      <GroupChannel
+        channelUrl={channelUrl || currentChannel.url}
+        key={channelUrl}
+        onChatHeaderActionClick={() => setShowSettings(true)}
+      />
+
+      {showSettings && (
+        <div className="absolute top-0 right-0 w-full h-full bg-black/50 flex justify-end rounded-xl">
+          <ChatSettingsSection
+            channelUrl={channelUrl}
+            onClose={() => setShowSettings(false)}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
